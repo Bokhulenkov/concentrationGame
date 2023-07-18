57ec01893d7edc1444ca153aef68a729cdcdf5e1
Bokhulenkov <bokhulenkov@gmail.com>
2023-07-18 10:07:12 +0300

    Add buttons

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            button.setTitle(" ", for: .normal)
+        } else {
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            button.setTitle(emoji, for: .normal)
+        }
     }
 
+    
+//    UI
 
+    
+    
+    
+    
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+    }
+    
 }
-
