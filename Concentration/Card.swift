0c7dadc8bf041e1d3e807e10e1f3a21d49412576
Bokhulenkov <bokhulenkov@gmail.com>
2023-07-19 12:31:45 +0300

    Create files model: Card& Concentration

---
+++ Concentration/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Alexander Bokhulenkov on 19.07.2023.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,16 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Alexander Bokhulenkov on 19.07.2023.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    func chooseCard(at index: Int){
+        
+    }
+} 
