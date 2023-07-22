b12566c3c0c892fd96e8b856e8c38531a5546f9d
Bokhulenkov <bokhulenkov@gmail.com>
2023-07-22 18:02:55 +0300

    Math index card

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,7 +10,27 @@ import Foundation
 class Concentration {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int){
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+//                check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+//                no cards or 2 cards are faceUp
+                for flipdownIndex in cards.indices {
+                    cards[flipdownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOrCards: Int) {
