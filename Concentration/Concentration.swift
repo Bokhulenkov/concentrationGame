26868e6e80fa9fe2fd1093bb194cb889a9bb36f8
Bokhulenkov <bokhulenkov@gmail.com>
2023-07-24 15:14:26 +0300

    Added Themes in game

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -37,8 +37,8 @@ class Concentration {
         for index in cards.indices {
             cards[index].isFaceUp = false
             cards[index].isMatched = false
-            cards.shuffle()
         }
+         cards.shuffle()
     }
     
     
